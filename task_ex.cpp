96eff91e6f241e3ea2d9d2519ad5bb90854c7bbd
Ajaypal11 <31420644+Ajaypal11@users.noreply.github.com>
2020-06-27 02:41:26 +0530

    Update task_ex.cpp

--- task_ex.cpp
+++ task_ex.cpp
@@ -1,49 +1,50 @@
 /*
     ajay_pal_11
-    sub-linear algo start
-    need to improve a lot 
+    improved
 */
 
 #include <cstdio>
 #include <map>
-#include <cmath>
 using namespace std;
 #define int64 long long int
 #define uint64 unsigned long long int
-static constexpr int LIMIT = 1e7 + 9 , mod = 1e9+7 ;
+static constexpr int LIMIT = 1e7 + 9 ;
+static constexpr uint64 add = -1 ;
 int phi[LIMIT] , primes[LIMIT] , sp[LIMIT] , sz;
-int64 tsum[LIMIT];
+uint64 tsum[LIMIT];
 map<int64,uint64> cache ;
-uint64 rangeSum(int64 L,int64 R){
-    uint64 ret = 1ULL * (R * (R+1)) - 1ULL * (L * (L-1));
-    return (ret/2);
-}
-uint64 smallN(int N){
-    int64 ret = 0 ;
-    for(int d=1;d<=N;d++){
-        ret += tsum[d] * (  (N/d) - (N/(d+1)));
-    }
+uint64 handle(uint64 x,uint64 y){
+    if(x>y)return x-y;
+    uint64 ret = add-y ;
+    ret += 1+x;
     return ret;
 }
-int64 LargeTotient(int64 N){
-
+uint64 rangeSum(uint64 L,uint64 R){
+    uint64 a,b;
+    if(R%2) a = ((R+1)/2) * R ;
+    else a = (R/2) * (R+1);
+    if(L%2) b = ((L-1)/2) * L ;
+    else b = (L/2) * (L-1);
+    return handle(a,b);
+}
+uint64 LargeTotient(int64 N){
     if(N<LIMIT) return tsum[N];
     if(cache.count(N))return cache[N];
-    uint64 ret = rangeSum(1,N) ;
-    int64 u,v;
-    int nsqrt = sqrt(N) , ymax = nsqrt;
-    if( nsqrt == int(N/nsqrt))--ymax;
-    for(int x=2;x<=nsqrt;x++) {
-        v = N/x;
-        u = cache[v] = LargeTotient(v);
-        ret -= u ;
+    uint64 agg=0 , cac , u , v , x = 2 ;
+    while (x<=N){
+        u = (N/x) ;
+        v = N/u ;
+        if( u<LIMIT )cac = tsum[u] ;
+        else if(cache.count(u)) cac = cache[u] ;
+        else cac = cache[u] = LargeTotient(u);
+        agg += (v-x+1) * cac ;
+        x = v + 1 ;
     }
-    for(int y=1;y<=ymax;y++)ret -= 1ULL * ( (N/y) - (N/(y+1))) * tsum[y] ;
-    return cache[N] = ret ;
+    return cache[N] = handle(rangeSum(1,N),agg) ;
 }
 uint64 solve(int64 N){
-    uint64 ret = 0 ;
-    int64 x = 1 , u , val ;
+    uint64 ret = 0 , val , u ;
+    int64 x = 1;
     while (x<=N){
         u = N/x ;
         if( u < LIMIT ){
@@ -53,10 +54,10 @@ uint64 solve(int64 N){
         }else{
             val = cache[u] = LargeTotient(u);
         }
-        ret += 1ULL * val * rangeSum(x,N/u);
+        ret += val * rangeSum(x,N/u);
         x = (N/u) + 1 ;
     }
-    return ret - (N * (N+1))/2 ;
+    return handle(ret,rangeSum(1,N));
 }
 int main(){
     phi[1] = sp[1] = tsum[1] = 1 ;
@@ -71,14 +72,11 @@ int main(){
         else phi[i] = phi[i/sp[i]] * sp[i] ;
         tsum[i] = tsum[i-1] + phi[i] ;
     }
-
     int tc;
     scanf("%d",&tc);
-    clock_t start = clock();
     while (tc--){
         int64 N ;
         scanf("%lld",&N);
         printf("%llu\n",solve(N));
     }
-    printf("%ld",(clock()-start)/CLOCKS_PER_SEC);
 }
